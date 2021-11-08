f525770a13910f6c03bb24a5182415605e9c5e2e
DeliteCode <80368625+DeliteCode@users.noreply.github.com>
2021-11-08 08:45:35 -0300

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AdressBook.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdressBook.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : 
+            base(options) { }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
